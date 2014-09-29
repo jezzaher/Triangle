492e61b0afb85b1722ea430e619d285cadd831bd
jezzaher <jezzaher@udel.edu>
2014-09-29 00:42:57 -0400

    Triangle

---
+++ Triangle/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		Triangle trial1=new Triangle();	
+		trial1.getSide1(1.0);
+		trial1.getSide2(1.0);
+		trial1.getSide3(1.0);
+		System.out.println(trial1.toString());
+		
+		
+	}
+}
